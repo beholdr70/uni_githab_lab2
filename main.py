c490439a33ce51b6043ee6084425c19648113f26
beholdr70 <alexsey.krivonogov@gmail.com>
2025-03-15 00:41:47 +0400

    Database branch 1st commit

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+def database_branch_function():
+    print('this database branch function: part 1')
+
+
 def main_branch_function():
     print('this is main branch original function')
 
 
 if __name__ == '__main__':
     main_branch_function()
+    database_branch_function()
 
