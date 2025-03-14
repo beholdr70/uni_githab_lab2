5cd680ef989ea4cabef30e373c1fea6144146d3b
beholdr70 <alexsey.krivonogov@gmail.com>
2025-03-15 00:48:22 +0400

    API branch 2nd commit

--- api_functional.py
+++ api_functional.py
@@ -1,3 +1,8 @@
-def api_branch_functional(parameter: list):
+def api_branch_function(parameter: list):
     for item in parameter:
         print(f'this item\'s index is {parameter.index(item)}')
+        api_branch_subfunction(item)
+
+
+def api_branch_subfunction(parameter):
+    print(f'this item\'s type is {type(parameter)}')
