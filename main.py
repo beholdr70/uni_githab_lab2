7aa8a63e8f3b4f2231838a3cc96600caf831fc6f
beholdr70 <alexsey.krivonogov@gmail.com>
2025-03-15 00:35:18 +0400

    Ui branch 1st commit

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+def ui_branch_function(parameter):
+    print(f'this is ui branch function: {len(str(parameter))}')
+
+
 def main_branch_function():
     print('this is main branch original function')
 
 
 if __name__ == '__main__':
     main_branch_function()
+    ui_branch_function('Hello, World!')
 
